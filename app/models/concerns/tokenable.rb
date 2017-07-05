74b26cfc30fb90a002ffab0340f594a9dc5d036e
Yammy <yammyxing@foxmail.com>
2017-07-06 00:28:42 +0800

    try concerns/tokenable

---
+++ app/models/concerns/tokenable.rb
@@ -0,0 +1,11 @@
+module Tokenable
+  # def generate_token
+  #   extend ActiveSupport::Concern
+  #
+  #   included do
+  #     before_validation :generate_token, :on => :create
+  #   end
+  #
+  #   self.token = SecureRandom.uuid
+  # end
+end
